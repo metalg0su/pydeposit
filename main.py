0ee483be1ac6db21eb905c678690047fa36ecefd
Seongjun Ji <metalg0su@gmail.com>
2020-10-05 16:53:48 +0900

    Polishing

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ logger.debug(f"===== Initialize User Defined Variables...")
 KEY_PATH = os.environ.get("KEY_PATH") or "./keystore"
 KEY_PASSPHRASE = os.environ.get("KEY_PASSPHRASE")
 CONTRACT_ADDR = os.environ.get("CONTRACT_ADDR")
-DEPOSIT_AMOUNT = os.environ.get("DEPOSIT_AMOUNT") or 5000
+DEPOSIT_AMOUNT = int(os.environ.get("DEPOSIT_AMOUNT")) or 5000
 
 if None in [KEY_PASSPHRASE, CONTRACT_ADDR, KEY_PATH, DEPOSIT_AMOUNT]:
     raise ValueError(f"Check variables.")
@@ -84,9 +84,10 @@ logger.debug(f"- Wallet Balance: {balance/ICX_FACTOR} ICX")
 
 logger.debug(f"===== Check pre-deposit values on contract {CONTRACT_ADDR}...")
 score_status = get_score_status(CONTRACT_ADDR)
-if score_status:
-    logger.debug(f"... Pre-deposited value found: {score_status}")
-    deposits: List[Dict[str, str]] = score_status["depositInfo"]["deposits"]
+pre_deposits = score_status.get("depositInfo")
+if pre_deposits:
+    logger.debug(f"... Pre-deposited value found: {pre_deposits}")
+    deposits: List[Dict[str, str]] = pre_deposits["deposits"]
 
     for each_deposit in deposits:
         deposit_id = each_deposit["id"]
@@ -94,10 +95,13 @@ if score_status:
         withdraw_result = withdraw(deposit_id)
         logger.debug(f"... Deposit ({deposit_id}) is in withdrawal: {withdraw_result}")
 
-logger.debug(f"===== Wait {WAIT_SEC} for reaching consensus...")
-time.sleep(WAIT_SEC)
+    logger.debug(f"===== Wait {WAIT_SEC} for reaching consensus...")
+    time.sleep(WAIT_SEC)
+else:
+    logger.debug(f"... Pre-deposited value not found.")
+
 
 logger.debug(f"===== Try to deposit assets: {DEPOSIT_AMOUNT} ICX...")
 add_result = add(DEPOSIT_AMOUNT)
 logger.debug(f"===== COMPLETE! Check on tracker: ")
-logger.debug(f"https://tracker.icon.foundation/transaction/{add_result.get('result')}")
+logger.debug(f"https://tracker.icon.foundation/transaction/{add_result}")
